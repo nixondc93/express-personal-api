90b2c73ce6764b7612bf1615ae3da75fbe7cc546
Derry Nixon <nixondc93@gmail.com>
2016-11-10 14:52:36 -0800

    test

--- models/index.js
+++ models/index.js
@@ -2,3 +2,11 @@ var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
 
 // module.exports.Campsite = require("./campsite.js.example");
+
+var CampsiteSchema = new Schema({
+  description: String
+});
+
+var Campsite = mongoose.model('Campsite', CampsiteSchema);
+
+module.exports = Campsite;
